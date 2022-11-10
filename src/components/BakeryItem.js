5b97e0811121e87186f499e30f9335a48a6cb4ba
largelamb38 <114973732+largelamb38@users.noreply.github.com>
2022-11-09 21:37:31 -0500

    Update BakeryItem.js

--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,15 @@
 // TODO: create a component that displays a single bakery item
+import { useId } from "react"
+
+// TODO: create a component that displays a single bakery item
+export default function BakeryItem(props) {
+    return (
+        <div>
+            {props.item.name}
+            <img src={props.item.image} />
+            {props.item.price}
+            <button onClick={() => props.updateCart(props.index)}> Add {props.item.name} to your cart!</button>
+        </div>
+    )
+    
+}
